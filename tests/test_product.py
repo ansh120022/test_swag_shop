5b182bcfa25050928d0a4effb97b2af93c3fcbe9
nsh12 <ansh120022@gmail.com>
2021-02-26 19:11:04 +0300

    Добавлены тесты страницы товара: переход на неё и обратно

--- tests/test_product.py
+++ tests/test_product.py
@@ -20,3 +20,18 @@ class TestProductPage:
 
         assert app.product_list.header() == "Products"
         assert len(product_names) > 0, "Товары не отображаются"
+
+    def test_cart(self, app):
+        """Добавление и удаление из корзины на этой странице"""
+        app.open_main_page()
+        app.login.do_login_standart()
+
+        # Добавление в корзину
+        app.product_list.item_name().click()
+        app.product.click_add_to_cart()
+
+        assert app.product.cart_counter() == 1
+
+        # удаление из корзины
+        app.product.click_add_to_cart()
+        assert app.product.cart_counter() == 0

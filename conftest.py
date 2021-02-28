11d4f01922d63664c0d5818e490145f657d9fc16
nsh12 <ansh120022@gmail.com>
2021-02-28 14:25:08 +0300

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -56,6 +56,6 @@ def pytest_addoption(parser):
     parser.addoption(
         "--headless",
         action="store",
-        default=False,
+        default=True,
         help="launching browser without gui",
     )
